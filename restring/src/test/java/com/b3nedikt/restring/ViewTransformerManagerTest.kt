67a123215cb38a6571a698b4b971234ac3c0297f
Benedikt <benedikt.loebbecke@tum.de>
2019-08-24 12:42:14 +0200

    Improved the ViewTransformerManager

--- restring/src/test/java/com/b3nedikt/restring/ViewTransformerManagerTest.kt
+++ restring/src/test/java/com/b3nedikt/restring/ViewTransformerManagerTest.kt
@@ -4,13 +4,13 @@ import android.util.AttributeSet
 import android.widget.TextView
 import androidx.test.core.app.ApplicationProvider
 import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.whenever
 import org.junit.Assert.assertSame
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mockito
-import org.mockito.Mockito.doReturn
 import org.robolectric.RobolectricTestRunner
 
 @RunWith(RobolectricTestRunner::class)
@@ -27,9 +27,11 @@ class ViewTransformerManagerTest {
     fun shouldTransformView() {
         val textView = TextView(ApplicationProvider.getApplicationContext())
 
-        val transformer = Mockito.mock(ViewTransformerManager.Transformer::class.java)
-        doReturn(TextView::class.java).`when`(transformer).viewType
+        val transformer = mock<ViewTransformerManager.Transformer>()
+
+        whenever(transformer.viewType).thenReturn(TextView::class.java)
         whenever(transformer.transform(any(), any())).thenReturn(textView)
+
         transformerManager.registerTransformer(transformer)
 
         val transformedView = transformerManager.transform(

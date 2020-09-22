e9c580da147e48e019760d3160a8adc8f360eec7
B3nedikt <mail@b3nedikt.dev>
2020-09-22 22:46:09 +0200

    Improved the internal repository implementation by extracting serialization logic for the Locales

--- restring/src/main/java/dev/b3nedikt/restring/internal/repository/persistent/Serializer.kt
+++ restring/src/main/java/dev/b3nedikt/restring/internal/repository/persistent/Serializer.kt
@@ -1,8 +1,18 @@
 package dev.b3nedikt.restring.internal.repository.persistent
 
+/**
+ * Serializes arbitrary objects of type [T] into objects of type [R], and vice versa for
+ * deserialization
+ */
 interface Serializer<T, R> {
 
+    /**
+     * Serialize a value of type [T] into a value of type [R]
+     */
     fun serialize(value: T): R
 
+    /**
+     * Deserialize a value of type [R] into a value of type [T]
+     */
     fun deserialize(value: R): T
 }
---
+++ restring/src/main/java/dev/b3nedikt/restring/internal/repository/persistent/SharedPrefsValueSetStore.kt
@@ -0,0 +1,72 @@
+package dev.b3nedikt.restring.internal.repository.persistent
+
+import android.content.SharedPreferences
+import dev.b3nedikt.restring.internal.repository.util.LocaleUtil
+
+/**
+ * [ValueSetStore] that stores value sets in the [SharedPreferences]
+ *
+ * @param sharedPreferences the [SharedPreferences] we store the values in
+ * @param serializer the [Serializer] used to serialize & deserialize a single element of the value
+ *  set
+ * @param stringKey the name of the preference where the value set will be stored to us a string set
+ */
+class SharedPrefsValueSetStore<E>(
+        private val sharedPreferences: SharedPreferences,
+        private val serializer: Serializer<E, String>,
+        private val stringKey: String
+) : ValueSetStore<E> {
+
+    override fun find(key: E): E? {
+        val localeString = sharedPreferences
+                .getStringSet(stringKey, null)
+                ?.find { LocaleUtil.fromLanguageTag(it) == key }
+                ?: return null
+
+        return serializer.deserialize(localeString)
+    }
+
+    override fun findAll(): Collection<E> {
+        return sharedPreferences
+                .getStringSet(stringKey, null)
+                ?.map { serializer.deserialize(it) }
+                ?: emptySet()
+    }
+
+    override fun save(element: E) {
+        val oldSet = findAll()
+        val newSet = (oldSet + element)
+                .map { serializer.serialize(it) }
+                .toSet()
+
+        sharedPreferences.edit()
+                .putStringSet(stringKey, newSet)
+                .apply()
+    }
+
+    override fun saveAll(elements: Collection<E>) {
+        val oldSet = findAll()
+        val newSet = (oldSet + elements)
+                .map { serializer.serialize(it) }
+                .toSet()
+
+        sharedPreferences.edit()
+                .putStringSet(stringKey, newSet)
+                .apply()
+    }
+
+    override fun delete(element: E) {
+        val oldSet = findAll()
+        val newSet = (oldSet - element)
+                .map { serializer.serialize(it) }
+                .toSet()
+
+        sharedPreferences.edit()
+                .putStringSet(stringKey, newSet)
+                .apply()
+    }
+
+    override fun deleteAll() {
+        sharedPreferences.edit().clear().apply()
+    }
+}

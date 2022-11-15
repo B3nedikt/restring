558facf0158741576610cdeb0611320f71565547
B3nedikt <mail@b3nedikt.dev>
2022-11-15 09:41:31 +0100

    Updated versions: kotlin, gradle, appCompat & test libs

--- buildSrc/src/main/kotlin/AppConfig.kt
+++ buildSrc/src/main/kotlin/AppConfig.kt
@@ -1,5 +1,5 @@
 object AppConfig {
-    const val compileSdk = 31
+    const val compileSdk = 32
     const val minSdk = 16
-    const val targetSdk = 31
+    const val targetSdk = 32
 }
--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,8 +1,8 @@
 object Dependencies {
 
-    const val kotlin = "org.jetbrains.kotlin:kotlin-stdlib:1.6.20"
+    const val kotlin = "org.jetbrains.kotlin:kotlin-stdlib:1.7.21"
 
-    const val appCompat = "androidx.appcompat:appcompat:1.4.0"
+    const val appCompat = "androidx.appcompat:appcompat:1.5.1"
     const val material = "com.google.android.material:material:1.4.0"
     const val constraintLayout = "androidx.constraintlayout:constraintlayout:2.0.4"
 
@@ -11,8 +11,8 @@ object Dependencies {
     const val reword = "dev.b3nedikt.reword:reword:4.0.2"
 
     const val junit = "junit:junit:4.13.2"
-    const val kluent = "org.amshove.kluent:kluent-android:1.68"
-    const val robolectric = "org.robolectric:robolectric:4.7.3"
+    const val kluent = "org.amshove.kluent:kluent-android:1.72"
+    const val robolectric = "org.robolectric:robolectric:4.9"
     const val mockitoKotlin = "org.mockito.kotlin:mockito-kotlin:4.0.0"
     const val mockitoInline = "org.mockito:mockito-inline:4.2.0"
     const val testCore = "androidx.test:core:1.4.0"

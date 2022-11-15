ebd69835570beb3a458fc45bcb04d54cb0496c2f
B3nedikt <mail@b3nedikt.dev>
2022-11-15 09:41:31 +0100

    Updated versions: kotlin, gradle, appCompat & test libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,8 +5,8 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.2.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.21")
+        classpath("com.android.tools.build:gradle:7.3.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.21")
     }
 }
 
--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -9,7 +9,7 @@ repositories {
 }
 
 dependencies {
-    implementation("com.android.tools.build:gradle:7.2.0")
-    implementation(kotlin("gradle-plugin", "1.6.20"))
+    implementation("com.android.tools.build:gradle:7.3.0")
+    implementation(kotlin("gradle-plugin", "1.7.21"))
     implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.6.21")
 }

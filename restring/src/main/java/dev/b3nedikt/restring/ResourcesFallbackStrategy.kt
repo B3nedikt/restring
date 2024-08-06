c7514446e4678eade765d0368610b791fccc6b3e
Benedikt Löbbecke <mail@b3nedikt.dev>
2024-08-06 19:22:19 +0200

    Added missing documentation and tests

--- restring/src/main/java/dev/b3nedikt/restring/ResourcesFallbackStrategy.kt
+++ restring/src/main/java/dev/b3nedikt/restring/ResourcesFallbackStrategy.kt
@@ -2,11 +2,24 @@ package dev.b3nedikt.restring
 
 import java.util.Locale
 
+/**
+ * A [ResourcesFallbackStrategy] defines the way restring decides what to do if a string
+ * cannot be found in it's [Restring.stringRepository]
+ */
 interface ResourcesFallbackStrategy {
 
+    /**
+     * Decides what to do if a string cannot be found in a [StringRepository]s strings
+     */
     fun getFallbackStringLocale(locale: Locale, stringKey: String): Locale?
 
+    /**
+     * Decides what to do if a string cannot be found in a [StringRepository]s quantityStrings
+     */
     fun getFallbackQuantityStringLocale(locale: Locale, stringKey: String): Locale?
 
+    /**
+     * Decides what to do if a string cannot be found in a [StringRepository]s stringArrays
+     */
     fun getFallbackStringArrayLocale(locale: Locale, stringKey: String): Locale?
 }
--- restring/src/main/java/dev/b3nedikt/restring/internal/DefaultResourcesFallbackStrategy.kt
+++ restring/src/main/java/dev/b3nedikt/restring/internal/DefaultResourcesFallbackStrategy.kt
@@ -9,20 +9,18 @@ import java.util.Locale
 internal object DefaultResourcesFallbackStrategy : ResourcesFallbackStrategy {
 
     override fun getFallbackStringLocale(locale: Locale, stringKey: String): Locale? {
-        if (locale.country.isNotEmpty()) {
-            return Locale(locale.language)
-        }
-        return null
+        return getFallbackLocale(locale)
     }
 
     override fun getFallbackQuantityStringLocale(locale: Locale, stringKey: String): Locale? {
-        if (locale.country.isNotEmpty()) {
-            return Locale(locale.language)
-        }
-        return null
+        return getFallbackLocale(locale)
     }
 
     override fun getFallbackStringArrayLocale(locale: Locale, stringKey: String): Locale? {
+        return getFallbackLocale(locale)
+    }
+
+    private fun getFallbackLocale(locale: Locale): Locale? {
         if (locale.country.isNotEmpty()) {
             return Locale(locale.language)
         }
